6ba89c6d175842b57e56ac2affd80b6cabdc04fc
sroybal2 <58920822+sroybal2@users.noreply.github.com>
2023-11-15 01:04:30 -0800

    Initial commit

---
+++ nix/machines/common/plex.nix
@@ -0,0 +1,77 @@
+# 🏴 + 🦜= 💰
+{ config, pkgs, lib, sensitive, ... }:
+let
+  pirateInterface = config.networking.wg-quick.interfaces ? pirate;
+  interface =
+    if pirateInterface then rec {
+      # For IP
+      endpoint = (lib.last config.networking.wg-quick.interfaces.pirate.peers).endpoint;
+      announced = builtins.elemAt (lib.splitString ":" endpoint) 0;
+
+      address = (lib.last config.networking.wg-quick.interfaces.pirate.address);
+      ipv4 = builtins.elemAt (lib.splitString "/" address) 0;
+      # Private block
+      ipv6 = "fe80::";
+    } else { };
+in
+{
+  environment.systemPackages = with pkgs; [ mediainfo ];
+
+  # Set group on the services to allow for file movement
+  users.users.transmission.extraGroups = [ "plex" "sonarr" "radarr" ];
+
+  # Testing. Is it transmission that does the moving?
+  users.users.sonarr.extraGroups = [ "plex" "transmission" ];
+  users.users.radarr.extraGroups = [ "plex" "transmission" ];
+
+  services = {
+    plex = {
+      enable = true;
+      openFirewall = true;
+      dataDir = "/media/plex";
+    };
+    sonarr = {
+      enable = true;
+      openFirewall = true;
+      group = "plex";
+    };
+    radarr = {
+      enable = true;
+      openFirewall = true;
+      group = "plex";
+    };
+    prowlarr = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    transmission = {
+      enable = true;
+      package = pkgs.transmission_4;
+      settings = {
+        download-dir = "/media/downloads/unsorted";
+        incomplete-dir = "/media/downloads/processing";
+        incomplete-dir-enabled = true;
+        rpc-whitelist = "127.0.0.1,192.168.*.*,10.13.37.*,10.1.1.*";
+        # Change listening ip
+        # rpc-bind-address = lib.mkForce interface.ipv4;
+        bind-address-ipv4 = lib.mkForce interface.ipv4;
+        bind-address-ipv6 = lib.mkForce interface.ipv6;
+        announce-ip = lib.mkForce interface.announced;
+        announce-ip-enabled = true;
+      };
+    };
+  };
+  systemd.services.plex.serviceConfig.KillSignal = lib.mkForce "SIGKILL";
+  # We have to hook in to set binds paths, since (undocumented), everything is
+  # RO except for a few whitelisted dirs. Fair, but frustrating without
+  # knowledge.
+  systemd.services.transmission.serviceConfig.BindPaths = [
+    "/media/downloads/shows"
+    "/media/downloads/movies"
+  ];
+
+  # A little bit of the personal config coming over. TODO: Create vpn-service
+  # hook in sensitive.
+  systemd.services.transmission.wantedBy = if pirateInterface then lib.mkForce [ "wg-quick-pirate.service" ] else [ ];
+}
---
+++ nix/overlays.nix
@@ -0,0 +1,62 @@
+{ sensitive }: [
+  (self: super: {
+    picom = super.picom.overrideAttrs (old: {
+      src = super.fetchFromGitHub {
+        owner = "jonaburg";
+        repo = "picom";
+        rev = "e3c19cd7d1108d114552267f302548c113278d45";
+        sha256 = "sha256-4voCAYd0fzJHQjJo4x3RoWz5l3JJbRvgIXn1Kg6nz6Y=";
+      };
+    });
+  })
+  (self: super:
+    let
+      pref = "extensions.zotero.dataDir";
+      path = "/home/${sensitive.lib.user}/.zotero/data";
+      pref2 = "extensions.zotero.useDataDir";
+    in
+    {
+      zotero = super.zotero.overrideAttrs (old: {
+        postPatch = old.postPatch + ''
+          sed -i '/pref("${pref}", .*);/c\pref("${pref}", "${path}");' defaults/preferences/zotero.js
+          sed -i '/pref("${pref2}", .*);/c\pref("${pref2}", true);' defaults/preferences/zotero.js
+        '';
+      });
+    })
+  (self: super: {
+    firefox = super.firefox.override {
+      extraPolicies = {
+        DontCheckDefaultBrowser = true;
+        DisablePocket = true;
+        Certificates = {
+          ImportEnterpriseRoots = true;
+          Install = self.lib.catAttrs "cert" (self.lib.attrValues sensitive.lib.certificates);
+        };
+      };
+    };
+  })
+  (_: pkgs: {
+    # ripped off nur/berbiche
+    mpvpaper = with pkgs; stdenv.mkDerivation rec {
+      pname = "mpvpaper";
+      version = "f65700a";
+
+      src = fetchFromGitHub {
+        owner = "GhostNaN";
+        repo = "mpvpaper";
+        rev = version;
+        hash = "sha256-h+YJ4YGVGGgInVgm3NbXQIbrxkMOD/HtBnCzkTcRXH8=";
+      };
+      nativeBuildInputs = [ pkg-config meson ninja cmake ];
+      buildInputs = [ wayland wayland-protocols mpv wlroots cairo ];
+      meta = {
+        description = ''
+          A wallpaper program for wlroots based Wayland compositors that
+          allows you to play videos with mpv as your wallpaper
+        '';
+        homepage = "https://github.com/GhostNaN/mpvpaper";
+        # license = licenses.gpl3;
+      };
+    };
+  })
+]
